0c81aa2ca583d37c34463aad409f907c6ab31e69
Kallu609 <khirvola@gmail.com>
2018-05-24 02:23:45 +0300

    Heroku support

--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,14 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 process.env.NTBA_FIX_319 = '1';
 
+import * as http from 'http';
 import Bot from './Bot';
 new Bot();
+
+// Heroku workaround
+// tslint:disable-next-line:no-any
+http.createServer((req: any, res: any) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
