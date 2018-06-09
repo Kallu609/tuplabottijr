a91a88f5b4d1cd1839c054e41db35ad50a46f3c6
Kallu609 <khirvola@gmail.com>
2018-06-09 19:15:27 +0300

    Removed heroku stuff

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,5 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 process.env.NTBA_FIX_319 = '1';
 
-import * as http from 'http';
 import Bot from './Bot';
 new Bot();
-
-// Heroku workaround
-http.createServer((req: any, res: any) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT || 5000);
